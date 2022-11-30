c23e376b277604a021d31a0c775bf235943bf019
Baran <baran.dgn@outlook.com>
2022-11-30 20:05:30 +0300

    Fix

--- app/src/main/java/com/example/moviews/view/MyListScreen.kt
+++ app/src/main/java/com/example/moviews/view/MyListScreen.kt
@@ -10,25 +10,29 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.example.moviews.R
+import com.example.moviews.model.MovieResponse
 import com.example.moviews.model.MovieResponseItems
 import com.example.moviews.util.Constrants.LOGO_URL
+import com.example.moviews.util.Resource
+import com.example.moviews.viewmodel.MyListViewModel
 
 @Composable
 fun MyListScreen(
-    orginal_title: String,
-    vote_average: Double,
-  //  poster_path: String,
-    navController: NavController
+
 ) {
+
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -37,47 +41,20 @@ fun MyListScreen(
             )
         }
     ) {
-        MyListRow(original_title = orginal_title, vote_average= vote_average)
+       // MyListRow(movieItem : MovieResponseItem)
     }
 
 
 }
-/*
-* LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            item {
-                MyListRow(original_title = orginal_title, vote_average = vote_average, poster_path = poster_path  , navController = navController)
-
-            }
-        }
-*
-* */
-
-
-
-
-
-
-
-
-
-
-
-
 
 @Composable
 fun MyListRow(
-    original_title : String,
-    vote_average : Double,
-  //  poster_path: String,
-    //movie: List<MovieResponseItems>,
-   // navController: NavController
+  movieItem : MovieResponseItems
 ){
     Box() {
         Row(modifier = Modifier.fillMaxWidth()) {
-            Image(painter = rememberImagePainter(data = R.drawable.movie_icon),
+
+          /*  Image(painter = rememberImagePainter(data = R.drawable.movie_icon),
                 contentDescription = "",
                 modifier = Modifier
                     .padding(8.dp)
@@ -93,13 +70,13 @@ fun MyListRow(
                     color = Color.Blue,
                     textAlign = TextAlign.Center)
 
-                Text(text = vote_average.toString(),
+                Text(text = .toString(),
                     style = MaterialTheme.typography.h6,
                     modifier = Modifier.padding(2.dp),
                     color = MaterialTheme.colors.primaryVariant,
-                    textAlign = TextAlign.Center)
+                    textAlign = TextAlign.Center)*/
 
-            }
+
             Box(
                 modifier = Modifier.padding(8.dp),
                 contentAlignment = Alignment.CenterEnd
@@ -110,8 +87,6 @@ fun MyListRow(
                 }
             }
         }
-
-
     }
 
 }
